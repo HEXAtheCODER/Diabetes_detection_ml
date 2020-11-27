f2c05e87419e10ac104e0be5b707d816f0588b3d
Pooshon Banerjee <pooshonbanerjee@in.ibm.com>
2020-11-27 12:44:37 +0530

    enhancements

--- flask_app.py
+++ flask_app.py
@@ -1,22 +1,33 @@
-from flask import Flask,render_template, request
+from flask import Flask,render_template, request, send_from_directory
 import numpy as np
 import joblib
+import os
+
 
 app = Flask(__name__)
 model = joblib.load("model")
 
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 @app.route('/')
 def hello():
     return render_template("index.html")
 
-@app.route('/result',methods=['POST'])
+@app.route('/detect')
+def detect():
+    return render_template("detect.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route('/result', methods=['POST'])
 def predict():
     list_of_features = [float(x) for x in request.form.values()]
     final_features = [np.array(list_of_features)]
     prediction = model.predict(final_features)
-
-
     return render_template("result.html",result = prediction[0])
 
 # IMPORTANT
@@ -24,5 +35,5 @@ def predict():
 # On heroku, the app is started by the command 'web: gunicorn flask_app:app'
 # If 'app.run()' is used, then the app will crash with error 'Address already in use', since gunicorn is already running
 # DO NOT UNCOMMENT THE FOLLOWING LINE WHEN PUSHING TO GIT
-
-# app.run(debug=True)
+# if __name__ == "__main__":
+#     app.run(host='0.0.0.0',debug=True)
